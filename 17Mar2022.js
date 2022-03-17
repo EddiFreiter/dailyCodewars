1c42788339235a80910847daf928230c2a329c06
Eduard Freiter <efreiter@edmontonexchanger.local>
2022-03-17 10:26:53 -0600

    17Mar2022 challange

--- 17Mar2022.js
+++ 17Mar2022.js
@@ -120,7 +120,6 @@ function pillars(num_pill, dist, width) {
 //     return num_pill>1?(num_pill-1)*dist*100+(num_pill-2)*width:0;
 //   }
 
-
 // TEST
 describe("Basic tests", function(){
     it("Testing for number of pillars: 1, distance: 10 m and width: 10 cm", function(){
