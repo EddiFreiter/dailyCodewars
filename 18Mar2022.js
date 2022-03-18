fea6d48e94fa35c7bc701c6beac543ae36c4993f
EddiFreiter <eddi@freiter.com>
2022-03-18 17:05:21 -0600

    Challenge 18 March 2022

--- 18Mar2022.js
+++ 18Mar2022.js
@@ -146,3 +146,39 @@ describe("Tests", () => {
   Test.assertEquals(findNeedle(haystack_3), 'found the needle at position 30')
     });
   });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// My Solution
+function maps(x){
+    let arr = []
+    x.forEach((element,index) => {
+        arr[index] = element * 2;
+    });
+    return arr
+}
+
+// Better Solution
+function maps(x){
+    return x.map(n => n * 2);
+  }
+
+// TEST
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", ()=>{
+  it("Fixed tests", () => {
+    assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+    assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]); 
+    assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]); 
+  });
+});
