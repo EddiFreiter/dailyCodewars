861fc74eabaca9902639759f2385b86b82a43cfb
Eddi Freiter <eddi@freiter.com>
2022-03-17 19:31:32 -0600

    Updated 17Mar2022

--- 17Mar2022.js
+++ 17Mar2022.js
@@ -30,7 +30,7 @@ describe("Tests", () => {
 // Write a function that will take the number of petals of each flower and
 // return true if they are in love and false if they aren't.
 
-// My Solution:
+// My Solution
 function lovefunc(flower1, flower2){
     // moment of truth
     function even(flwr){
