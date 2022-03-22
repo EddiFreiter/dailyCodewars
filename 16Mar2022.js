40cad239f42d1962bb657ab4aa4eed4c9fdcf2bf
Eddi Freiter <eddi@freiter.com>
2022-03-21 22:56:52 -0600

    Challenge - 16 March 2022

--- 16Mar2022.js
+++ 16Mar2022.js
@@ -40,6 +40,8 @@ describe("Basic tests", () => {
 // makeNegative(-5);   // return -5
 // makeNegative(0);    // return 0
 // makeNegative(0.12); // return -0.12
+
+// My Solution
 function makeNegative(num) {
     // Code?
   if (num <= 0){
