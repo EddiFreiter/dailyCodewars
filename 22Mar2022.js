8a442cd58acf2e09b137b0274d01dce1ac911ec4
Eddi Freiter <eddi@freiter.com>
2022-03-26 22:58:37 -0600

    Challenge - 22 March 2022

--- 22Mar2022.js
+++ 22Mar2022.js
@@ -1,18 +1,18 @@
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// // Count of positives / sum of negatives
+// Count of positives / sum of negatives
 
-// // Given an array of integers.
+// Given an array of integers.
 
-// // Return an array, where the first element is the count of positives
-// // numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// Return an array, where the first element is the count of positives
+// numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
-// // If the input is an empty array or is null, return an empty array.
+// If the input is an empty array or is null, return an empty array.
 
-// // Example
-// // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
-// // My Solution
+// My Solution
 function countPositivesSumNegatives(input) {
     if (input != null && input.length != 0) {
         sumPos = 0
@@ -23,28 +23,28 @@ function countPositivesSumNegatives(input) {
     return []; 
 }
 
-// Other, complet Solution
+Other, complet Solution
 function countPositivesSumNegatives(input) {
     return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
 }
 
-// // TEST
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+// TEST
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
 
-// describe("Example tests", () => {
-//   it("Testing for fixed test 1", () => {
-//     let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
-//     let actual = countPositivesSumNegatives(testData);
-//     let expected = [10, -65];
-//     assert.deepEqual(actual, expected);
-//   })
+describe("Example tests", () => {
+  it("Testing for fixed test 1", () => {
+    let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+    let actual = countPositivesSumNegatives(testData);
+    let expected = [10, -65];
+    assert.deepEqual(actual, expected);
+  })
     
-//   it("Testing for fixed test 1", () => {
-//     let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
-//     let actual = countPositivesSumNegatives(testData);
-//     let expected = [8, -50];    
-//     assert.deepEqual(actual, expected);
-//   });
-// });
+  it("Testing for fixed test 1", () => {
+    let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+    let actual = countPositivesSumNegatives(testData);
+    let expected = [8, -50];    
+    assert.deepEqual(actual, expected);
+  });
+});
